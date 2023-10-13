5da1e2ea6ec4c3155eacf6b065e27e93db133620
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-13 12:28:46 -0400

    Preparing for the CCC Contest

---
+++ CodeWars/8kyu/CollatzConjecture/CollatzConjecture.cpp
@@ -0,0 +1,21 @@
+
+#include "CollatzConjecture.h"
+#include <iostream>
+
+using namespace std;
+/*
+ *
+The Collatz conjecture (also known as 3n+1 conjecture) is a conjecture that applying the following algorithm to any number we will always eventually reach one:
+
+[This is writen in pseudocode]
+if(number is even) number = number / 2
+if(number is odd) number = 3*number + 1
+
+*/
+int main() {
+    int number = 0;
+
+    cout << endl;
+
+    return 0;
+}
---
+++ CodeWars/8kyu/CollatzConjecture/CollatzConjecture.h
@@ -0,0 +1,14 @@
+//
+// Created by aryan on 2023-10-13.
+//
+
+#ifndef CCCPREP_COLLATZCONJECTURE_H
+#define CCCPREP_COLLATZCONJECTURE_H
+
+
+class CollatzConjecture {
+
+};
+
+
+#endif //CCCPREP_COLLATZCONJECTURE_H
