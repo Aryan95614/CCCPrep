1c0bcdea450dcb2f0d463954c764648312fb3ef8
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-20 16:21:02 -0400

    Directly added problem set here

---
+++ CCC/CCC2023/playground.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<iomanip>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define vi vector<int>
+#define into cin >>
+#define print cout <<
+
+int main () {
+    int numMountains;
+    into numMountains;
+    vi heights = vi{};//vi{3, 1, 4, 1, 5, 9, 2};
+
+    for (int i = 0; i < numMountains; i++) {
+        int thing = 0;
+        into thing;
+        heights.push_back(thing);
+    }
+
+
+
+
+
+
+    return 0;
+}
