bf7c446cd4837e05c9cf0605845a5d83a57981c8
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-30 20:16:46 -0400

    WOrk

--- CCC/CCC2023/prob2.java
+++ CCC/CCC2023/prob2.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class prob2 {
     public static Vector<Integer> subsize(int[] vector2, int size, int starting) {
         Vector<Integer> vector1 = new Vector<Integer>();
-        for(int n: vector2) {
+        for (int n : vector2) {
             vector1.add(n);
         }
         Vector<Integer> temp = new Vector<Integer>();
@@ -25,7 +25,7 @@ public class prob2 {
         if (size == 1) {
             return 0;
         } else {
-            limit = (size % 2 == 0) ? size / 2: ((size - 1) / 2);
+            limit = (size % 2 == 0) ? size / 2 : ((size - 1) / 2);
         }
 
 
@@ -36,6 +36,7 @@ public class prob2 {
 
         return value;
     }
+
     public static void main(String[] args) {
         final int numMountains = 8;
         int[] arr = {2, 4, 9, 12, 15, 20, 24, 30};
@@ -43,7 +44,7 @@ public class prob2 {
         Vector<int[]> values = new Vector<>();
         // -----
 
-        int[] bottom = new int[arr.length-1];
+        int[] bottom = new int[arr.length - 1];
 
         for (int i = 0; i < bottom.length; i++) {
             bottom[i] = asymmetric(subsize(arr, 2, i));
@@ -51,27 +52,45 @@ public class prob2 {
         }
 
 
-        for (int element: bottom) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
+        values.add(bottom);
 
-        // ---------------------------------
-
-        int[] bottom1 = new int[arr.length-2];
-
-        for (int i = 0; i < bottom.length-1; i++) {
-            bottom1[i] = bottom[i] + bottom[i+1];
-
-        }
-
-
-        for (int element: bottom1) {
+        for (int element : bottom) {
             System.out.print(element + " ");
         }
 
 
+        System.out.println();
+        int[] bottom1 = new int[arr.length - 2];
+
+        for (int i = 0; i < bottom.length - 1; i++) {
+            bottom1[i] = bottom[i] + bottom[i + 1];
+
+        }
+
+
+        for (int element : bottom1) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        values.add(bottom1);
+
+        for (int i = 0; i < 5; i++) {
+            int value = values.get(0)[1+i];
+
+            value += arr[3+i];
+            value -= arr[i];
+            System.out.println(value);
+        }
 
         System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            int value = values.get(1)[1+i];
+
+            value += arr[4+i];
+            value -= arr[i];
+            System.out.println(value);
+        }
+
     }
 }
