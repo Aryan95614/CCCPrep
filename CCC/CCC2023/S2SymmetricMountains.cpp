fe305a04ae56141d6130861653a3aaf6f6c6cbf0
Aryan95614 <64114240+Aryan95614@users.noreply.github.com>
2023-10-31 00:05:56 -0400

    This is ending itself

--- CCC/CCC2023/S2SymmetricMountains.cpp
+++ CCC/CCC2023/S2SymmetricMountains.cpp
@@ -1,3 +1,4 @@
+
 //
 // Created by aryan on 2023-10-18.
 //
@@ -10,17 +11,23 @@
 
 using namespace std;
 
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+#define fastio ios_base::sync_with_stdio(0);
 #define vi vector<int>
-#define into cin >>
 #define print cout <<
+#define newline cout << endl;
 
-vi subsize(vi& vector1, int size, int starting) {
+void printarray(int example[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << example[i] << " " << std::flush;
+    }
+}
 
-    if (starting < vector1.size()) {
+vi subsize(int vector1[], int size, int starting) {
+
+    if (starting < size) {
         vi temp = vi{};
-        for(int i = 0; i < size; i++) {
-            temp.push_back(vector1.at(i+starting));
+        for (int i = 0; i < size; i++) {
+            temp.push_back(vector1[i + starting]);
         }
         return temp;
     }
@@ -29,73 +36,83 @@ vi subsize(vi& vector1, int size, int starting) {
 
 }
 
-int asymmetric(vi subset) {
+int asymmetric(int arr[], int size) {
     int value = 0;
-    int size = (int)subset.size();
+    int limit;
+
+
     if (size == 1) {
         return 0;
-    }
-
-    if (size % 2 == 1) {
-
-        for(int i = 0; i < (size-1)/2;i++ ) {
-
-            value += abs(subset.at(i) - subset.at(size-i-1));        }
-
-        return value;
     } else {
-        for(int i = 0; i < size/2;i++ ) {
-
-            value += abs(subset.at(i) - subset.at(size-i-1));      }
-
-        return value;
+        limit = size / 2 ? (size % 2 == 0) : ((size - 1) / 2);
     }
 
 
+    for (int i = 0; i < limit; i++) {
+
+        value += abs(arr[i] - arr[size - i - 1]);
+    }
+
+    return value;
+
+
 }
 
-int getLimit(int number, int limit) {
-    return limit-number+1;
-}
-
-int main () {
+int main() {
     fastio;
 
     int numMountains;
-    into numMountains;
-    cout << endl;
+    cin >> numMountains;
+    vi heights = vi{};//vi{3, 1, 4, 1, 5, 9, 2};
 
-    if (numMountains == 0 or numMountains == 1) {
-        cout << 0 << endl;
-        return 0;
+    string answer;
+    cin >> ws;
+    getline(cin, answer);
+
+    answer.push_back(' ');
+    answer.push_back('0');
+
+    char delim = ' ';
+
+
+    for (;;){//int i = 0; i <numMountains;i++) {
+        // Split the answer string by delimiter
+        vector<int> results;
+        stringstream ss(answer);
+
+        string token;
+        while (getline(ss, token, delim)) {
+            int it = stoi(token);
+            results.push_back(it);
+        }
+
+        // Ensure we have at least two values
+        if (results.size() <= 2) {
+            break;
+        }
+        int input = results[0];
+
+        // Remove processed part of the answer string
+        answer = answer.substr(answer.find(delim) + 1);
+
+        heights.push_back(input);
+
     }
-    vi heights = vi{};
-
-    for (int i = 0; i < numMountains; i++) {
-        int thing = 0;
-        into thing;
-        heights.push_back(thing);
-    }
-
     vi outputs = vi{0};
 
+    int bottom[numMountains-1];
+    int bottomSize = sizeof(bottom)/sizeof(bottom[0]);
 
 
-    for (int sizes = 2; sizes < numMountains; sizes++) {
-        vi temp1 = vi{};
-        for (int startings = 0; startings < getLimit(sizes, numMountains); startings += 1) {
-            int element = 0;
-            vi temp = subsize(heights, sizes, startings);
-            temp1.push_back(asymmetric(temp));
-        }
-        outputs.push_back(*min_element(temp1.begin(), temp1.end()));
+
+    for (int i = 0; i < bottomSize; i++) {
+        bottom[i] = abs(outputs[i+1] - outputs[i]);
+
     }
 
-    outputs.push_back(asymmetric(heights));
-
-
-    for (int element: outputs) {
+    for (int element: bottom) {
         cout << element << " " << std::flush;
     }
 
+    return 0;
 }
